101bf2fb6d8f4b542db45283fe29a5a4bb5d3f07
Tnze <cjd001113@outlook.com>
2020-02-24 01:15:04 +0800

    调整项目结构，把可执行程序单独放在一个目录里，core目录作为一个lib

--- core/ChineseChess.cpp
+++ core/ChineseChess.cpp
@@ -1,12 +1,5 @@
-﻿// ChineseChess.cpp: 定义应用程序的入口点。
-//
+﻿#include "ChineseChess.h"
 
-#include "ChineseChess.h"
+namespace NormChineseChess {
 
-using namespace std;
-
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
 }
---
+++ textui/Main.cpp
@@ -0,0 +1,10 @@
+#include "../core/ChineseChess.h"
+
+using namespace std;
+
+int main()
+{
+	auto g = new NormChineseChess::Game();
+	cout << "Hello Chess." << endl;
+	return 0;
+}
