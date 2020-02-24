48b139e63c7785e88da0908d028544f7edd00cf9
Tnze <cjd001113@outlook.com>
2020-02-24 17:21:38 +0800

    把Main.cpp改为用UTF-8编码

--- textui/Main.cpp
+++ textui/Main.cpp
@@ -7,23 +7,23 @@ int main()
 {
 	cout << "Hello Chess." << endl;
 	Game* g = new Game();
-	// 双方棋手入场
+	// 鍙屾柟妫嬫墜鍏ュ満
 
-	// 双方棋手轮流落子
+	// 鍙屾柟妫嬫墜杞祦钀藉瓙
 	for (bool player = true; g->GetStatus() == Status::Gaming; player = !player) {
 		Move m;
 		if (player)
-			m = Move();// 红方
+			m = Move();// 绾㈡柟
 		else
 			m = Move();
 
 
 	}
-	// 公布胜负
+	// 鍏竷鑳滆礋
 	if (g->GetStatus() == Status::RedWin)
-		cout << "红方获胜!" << endl;
+		cout << "绾㈡柟鑾疯儨!" << endl;
 	else
-		cout << "黑方获胜!" << endl;
+		cout << "榛戞柟鑾疯儨!" << endl;
 	delete g;
 	return 0;
 }
